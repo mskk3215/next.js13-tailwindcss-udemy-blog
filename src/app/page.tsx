b7f9e3b9931d6447dd3ad2b34111b44bf6f4cd87
mskk3215 <m.kaaiweb@gmail.com>
2023-12-02 18:36:22 +0900

    feat:"ArticleList" add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <></>;
+  return (
+    <div className="md:flex">
+      <section className="w-full md:w-2/3 flex flex-col items-center px-3">
+        <ArticleList />
+      </section>
+
+    </div>
+  );
 }
