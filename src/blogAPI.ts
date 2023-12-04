1f555195275bebd562dccb4842e893a74f477756
mskk3215 <m.kaaiweb@gmail.com>
2023-12-04 23:19:28 +0900

    feat:API about Post add&import&setting by CSR

--- src/blogAPI.ts
+++ src/blogAPI.ts
@@ -31,3 +31,27 @@ export const getDetailArticle = async (id: string): Promise<Article> => {
   const article = await res.json();
   return article;
 };
+
+export const createArticle = async (
+  id: string,
+  title: string,
+  content: string
+): Promise<Article> => {
+  const currentDateTime = new Date().toISOString();
+  const res = await fetch(`http://localhost:3001/posts`, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({ id, title, content, currentDateTime }),
+  });
+
+  if (!res.ok) {
+    throw new Error("エラーが発生しました");
+  }
+
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  const newArticle = await res.json();
+  return newArticle;
+};
