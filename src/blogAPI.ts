4bbd5b38a1b32d16b5df84f42ba51bba9d35172f
mskk3215 <m.kaaiweb@gmail.com>
2023-12-04 22:09:20 +0900

    feat:NotFound page add

--- src/blogAPI.ts
+++ src/blogAPI.ts
@@ -1,3 +1,4 @@
+import { notFound } from "next/navigation";
 import { Article } from "./types";
 
 export const getAllArticles = async (): Promise<Article[]> => {
@@ -16,6 +17,11 @@ export const getDetailArticle = async (id: string): Promise<Article> => {
   const res = await fetch(`http://localhost:3001/posts/${id}`, {
     next: { revalidate: 60 },
   }); //ISR
+
+  if (res.status === 404) {
+    notFound();
+  }
+
   if (!res.ok) {
     throw new Error("エラーが発生しました");
   }
