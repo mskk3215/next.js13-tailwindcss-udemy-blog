31ec472e7d9405681598ec6ec709509c2f73092a
mskk3215 <m.kaaiweb@gmail.com>
2023-12-04 21:43:57 +0900

    feat:API about detailArticle add&import&setting by ISR

--- src/blogAPI.ts
+++ src/blogAPI.ts
@@ -11,3 +11,17 @@ export const getAllArticles = async (): Promise<Article[]> => {
   const articles = await res.json();
   return articles;
 };
+
+export const getDetailArticle = async (id: string): Promise<Article> => {
+  const res = await fetch(`http://localhost:3001/posts/${id}`, {
+    next: { revalidate: 60 },
+  }); //ISR
+  if (!res.ok) {
+    throw new Error("エラーが発生しました");
+  }
+
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  const article = await res.json();
+  return article;
+};
